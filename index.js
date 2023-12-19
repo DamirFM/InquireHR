9089bb9a25881a768724c1a05aa6476be4c2d4df
DamirFM <yakupovdamir93@gmail.com>
2023-12-18 22:02:55 -0500

    added delete function

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ const db = mysql.createConnection(
 db.connect(() => {
     mainMenu();
 });
+// Back to main menu function
+function confirmReturnToMainMenu() {
+    return inquirer.prompt({
+        type: 'confirm',
+        name: 'confirmation',
+        message: 'return to the main menu?',
+        default: true 
+    });
+}
 // function for display MAIN MENU and take user input in terminal 
 function mainMenu() {
     inquirer
@@ -26,7 +35,16 @@ function mainMenu() {
                 type: 'list',
                 message: 'What would you like to do?',
                 name: 'menu',
-                choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+                choices: [
+                'view all departments', 
+                'view all roles', 
+                'view all employees', 
+                'add a department', 
+                'add a role', 
+                'add an employee', 
+                'update an employee role',
+                'delete a department', 
+                'exit']
             },
         ])
         .then(data => {
@@ -58,6 +76,13 @@ function mainMenu() {
                 // call function for update a employee role
                 updateEmployeeRole();
             }
+            else if (data.menu === 'delete a department') {
+                // call function for update a employee role
+                deleteDepartment();
+            }
+            else if (data.menu === 'exit') {
+                exitApp();
+            }
         })
 };
 // function for view all departments
@@ -69,8 +94,14 @@ function viewDepartments() {
         // If there's an error during the query execution, it might be captured in the err parameter. 
         // Otherwise, if the query is successful, data contains the retrieved records.
         printTable(data);
-        // return MAIN MENU back
-        mainMenu();
+        confirmReturnToMainMenu().then(answer => {
+            if (answer.confirmation) {
+                // User confirmed, go back to the main menu
+                mainMenu();
+            } else {
+                viewDepartments();
+            }
+        });
     })
 };
 function viewEmployees() {
@@ -81,7 +112,15 @@ function viewEmployees() {
     LEFT JOIN department on role.department_id = department.id
     LEFT JOIN employee m on employee.manager_id = m.id`, (err, data) => {
         printTable(data);
-        mainMenu();
+        // Back to main menu option
+        confirmReturnToMainMenu().then(answer => {
+            if (answer.confirmation) {
+                // User confirmed, go back to the main menu
+                mainMenu();
+            } else {
+                viewEmployees();
+            }
+        });
     })
 };
 
@@ -90,7 +129,14 @@ function viewRoles() {
     SELECT role.id, title, salary, name department_name
     FROM role LEFT JOIN department on role.department_id = department.id`, (err, data) => {
         printTable(data);
-        mainMenu();
+        confirmReturnToMainMenu().then(answer => {
+            if (answer.confirmation) {
+                // User confirmed, go back to the main menu
+                mainMenu();
+            } else {
+                viewRoles();
+            }
+        });
     })
 };
 
@@ -183,3 +229,40 @@ function updateEmployeeRole() {
             })
         })
 };
+// function for view all departments
+function deleteDepartment() {
+
+
+
+    db.query(`SELECT * FROM department`, (err, data) => {
+        // This is the callback function that handles the result of the query
+        // If there's an error during the query execution, it might be captured in the err parameter. 
+        // Otherwise, if the query is successful, data contains the retrieved records.
+        printTable(data);
+    });
+
+
+let del;
+    inquirer.prompt({
+        type: 'input',
+        message: 'delete department\n',
+        name: 'delete',
+    })
+        .then(data => {
+            data.delete = del;
+            // This handles the user's response once they input the new department name. 
+            // The user's input is captured in the data object
+
+            // db.query(): This method is used to execute SQL queries against the connected database. It usually takes several parameters:
+            // The first parameter is the SQL query string to be executed.
+            // The second parameter is an array containing values to be inserted into the query. These values replace placeholders (in this case, the ? in the query string). [data.name] contains the user-provided department name to be inserted.
+            // The third parameter is a callback function that will be executed after the query is performed. This function usually takes an err parameter to handle any errors that might occur during the query execution.
+            db.query('DELETE FROM department WHERE id = ?', del, (err, data) => {
+                printTable(data);
+            })
+        })
+};
+function exitApp() {
+    console.log("Exiting the application...");
+    process.exit(0); // 0 indicates a successful exit, while other values can indicate an error or specific exit status
+}
